433d692e85b372f6e8c0bc2602cd576d76adfdae
justinrsmith <justinrsmith88@gmail.com>
2013-11-28 15:35:08 -0600

    more clean up

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import oauth2 as oauth
-import urllib
-consumer = oauth.Consumer('zq33ap8e526smhskzx7xkghf', 'rudg3ASW2T')
-client = oauth.Client(consumer)
-response = client.request('http://api.rdio.com/1/', 'POST', urllib.urlencode({'method': 'get', 'artist': 'arcade fire'}))
-print response[1]
