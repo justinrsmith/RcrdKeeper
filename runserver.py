2b5741eb019e689a4805f34160a0c678e074629c
justinrsmith <justinrsmith88@gmail.com>
2013-11-30 12:54:59 -0600

    handling for missng albums, code refactor

--- runserver.py
+++ runserver.py
@@ -69,41 +69,7 @@ def get_records():
 @app.route('/submit', methods=['POST', 'GET'])
 def new_record():
 
-    records = r.db('rcrdkeeprapp').table('records')
-
-    #query for album info
-    album_info = rdio.call('search', {'query': request.form['album'],
-                                         'types': 'album'})
-
-    #retrieve album artwork
-    for x in album_info['result']['results']:
-        if x['artist'] == request.form['artist']:
-            album_art = x['icon']
-            release_date = x['releaseDate']
-            duration = x['duration']
-            duration = duration/60
-            tracks = x['length']
-            artist_key = x['artistKey']
-
-#    album_info_extend = rdio.call('getAlbumsForArtist', 
-#                                {'query': artist_key, 'artist': artist_key})
-
-    new_info = [{'artist': request.form['artist'],
-                        'album': request.form['album'],
-                            'album art': album_art}]
-
-    #insert album info
-    succ = records.insert([{'artist': request.form['artist'],
-                        'album': request.form['album'],
-                            'album art': album_art,
-                            'release_date': release_date,
-                            'duration': duration,
-                            'tracks': tracks,
-                            'record_condition': '',
-                            'sleeve_condition': '',
-                            'color': '',
-                            'size': '',
-                            'notes': ''}]).run(g.rdb_conn)
+    new_info = query(request.form, 'insert')
 
     return render_template('new_record.html',
                             new_info=new_info)
@@ -112,17 +78,10 @@ def new_record():
 @app.route('/edit', methods=['POST'])
 def edit_record():
 
-    print request.form
-    records = r.db('rcrdkeeprapp').table('records')
+    new_info = query(request.form, 'edit')
 
-    records.get(request.form['id']).update({
-                    'record_condition': request.form['record_condition'],
-                    'sleeve_condition': request.form['sleeve_condition'],
-                    'color': request.form['color'],
-                    'notes': request.form['notes'],
-                    'size' : request.form['size']}).run(g.rdb_conn)
-
-    return ''
+    return render_template('new_record.html',
+                            new_info=new_info)
 
 
 @app.route('/delete/<string:record_id>', methods=['POST'])
@@ -133,6 +92,76 @@ def delete_record(record_id=None):
     records.get(record_id).delete().run(g.rdb_conn)
 
     return ''
+
+def query(form, query_type):
+
+    records = r.db('rcrdkeeprapp').table('records')
+
+    album_info = rdio.call('search', {'query': form['album'],
+                                         'types': 'album'})
+
+    print album_info
+
+    if album_info['result']['number_results'] != 0:
+        for x in album_info['result']['results']:
+            print 'for'
+            if x['artist'] == form['artist']:
+                album_art = x['icon']
+                release_date = x['releaseDate']
+                duration = x['duration']
+                duration = duration/60
+                tracks = x['length']
+                artist_key = x['artistKey']
+                break
+            else:
+                album_art = 'http://musicunderfire.com/wp-content/uploads/2012/06/No-album-art-itunes-300x300.jpg'
+                release_date = ''
+                duration = 0
+                tracks = 0
+                artist_key = ''
+    else:
+        album_art = 'http://musicunderfire.com/wp-content/uploads/2012/06/No-album-art-itunes-300x300.jpg'
+        release_date = ''
+        duration = 0
+        tracks = 0
+        artist_key = ''
+
+    if query_type == 'insert':
+        succ = records.insert([{'artist': form['artist'],
+                            'album': form['album'],
+                                'album art': album_art,
+                                'release_date': release_date,
+                                'duration': duration,
+                                'tracks': tracks,
+                                'record_condition': '',
+                                'sleeve_condition': '',
+                                'color': '',
+                                'size': '',
+                                'notes': ''}]).run(g.rdb_conn)
+
+        return [{'artist': form['artist'],
+                        'album': form['album'],
+                            'album art': album_art}]
+    elif query_type == 'edit':
+        records.get(request.form['id']).update({
+                        'artist': form['artist'],
+                        'album': form['album'],
+                        'album art': album_art,
+                        'release_date': release_date,
+                        'duration': duration,
+                        'tracks': tracks,
+                        'record_condition': form['record_condition'],
+                        'sleeve_condition': form['sleeve_condition'],
+                        'color': form['color'],
+                        'notes': form['notes'],
+                        'size' : form['size']}).run(g.rdb_conn)
+        return [{'artist': form['artist'],
+                        'album': form['album'],
+                            'album art': album_art}]
+    else:
+        print 'failure'
+
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run the Flask todo app')
