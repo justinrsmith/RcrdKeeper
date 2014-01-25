03a92aa9a314d2a903407480995b93eda1632931
justinrsmith <justinrsmith88@gmail.com>
2014-01-25 10:24:47 -0600

    fix setup db code

--- rcrdkeepr/views.py
+++ rcrdkeepr/views.py
@@ -18,29 +18,6 @@ RCRDKEEPR_DB = 'rcrdkeeprapp'
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-def dbSetup():
-    connection = r.connect(host='localhost', port=28015)
-    try:
-        r.db_create(RCRDKEEPR_DB).run(connection)
-        r.db(RCRDKEEPR_DB).table_create('records').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('users').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('records').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('record_condition').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('record_size').run(connection)
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'VG','condition':'Very Good', 'order': 1})
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'G','condition':'Good', 'order': 2})
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'AV','condition':'Average', 'order': 3})
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'P','condition':'Poor', 'order': 4})
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'VP','condition':'Very Poor', 'order': 5})
-        r.db('rcrdkeeprapp').table('record_size').insert({'size':'12 inch', 'order': 1})
-        r.db('rcrdkeeprapp').table('record_size').insert({'size':'10 inch', 'order': 2})
-        r.db('rcrdkeeprapp').table('record_size').insert({'size':'7 inch', 'order': 3})
-        print 'Database setup completed. Now run the app without --setup.'
-    except RqlRuntimeError:
-        print 'App database already exists. Run the app without --setup.'
-    finally:
-        connection.close()
-
 users = r.db('rcrdkeeprapp').table('users')
 records = r.db('rcrdkeeprapp').table('records')
 
--- runserver.py
+++ runserver.py
@@ -1,6 +1,29 @@
 from rcrdkeepr import app
 import argparse
-   
+  
+def dbSetup():
+    connection = r.connect(host='localhost', port=28015)
+    try:
+        r.db_create(RCRDKEEPR_DB).run(connection)
+        r.db(RCRDKEEPR_DB).table_create('records').run(connection)
+        r.db(RCRDKEEPR_DB).table_create('users').run(connection)
+        r.db(RCRDKEEPR_DB).table_create('records').run(connection)
+        r.db(RCRDKEEPR_DB).table_create('record_condition').run(connection)
+        r.db(RCRDKEEPR_DB).table_create('record_size').run(connection)
+        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'VG','condition':'Very Good', 'order': 1})
+        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'G','condition':'Good', 'order': 2})
+        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'AV','condition':'Average', 'order': 3})
+        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'P','condition':'Poor', 'order': 4})
+        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'VP','condition':'Very Poor', 'order': 5})
+        r.db('rcrdkeeprapp').table('record_size').insert({'size':'12 inch', 'order': 1})
+        r.db('rcrdkeeprapp').table('record_size').insert({'size':'10 inch', 'order': 2})
+        r.db('rcrdkeeprapp').table('record_size').insert({'size':'7 inch', 'order': 3})
+        print 'Database setup completed. Now run the app without --setup.'
+    except RqlRuntimeError:
+        print 'App database already exists. Run the app without --setup.'
+    finally:
+        connection.close()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run the Flask todo app')
     parser.add_argument('--setup', dest='run_setup', action='store_true')
