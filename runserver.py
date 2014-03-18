ed252b9ac90757ddd66f901c1f8e479778b8afc4
justinrsmith <justinrsmith88@gmail.com>
2014-03-17 20:09:19 -0500

    renames

--- runserver.py
+++ runserver.py
@@ -1,29 +1,29 @@
-from rcrdkeepr import app
+from rcrdkeeper import app
 import argparse
 import rethinkdb as r
 from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 
 
-RCRDKEEPR_DB = 'rcrdkeeprapp'
+RCRDKEEPER_DB = 'rcrdkeeper'
 
 def dbSetup():
     connection = r.connect(host='localhost', port=28015)
     try:
-        r.db_create(RCRDKEEPR_DB).run(connection)
-        r.db(RCRDKEEPR_DB).table_create('records').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('users').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('record_condition').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('record_size').run(connection)
-        r.db(RCRDKEEPR_DB).table_create('contact').run(connection)
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'M','condition':'Mint', 'order': 1}).run(connection)
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'VG','condition':'Very Good', 'order': 2}).run(connection)
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'G','condition':'Good', 'order': 3}).run(connection)
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'AV','condition':'Average', 'order': 4}).run(connection)
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'P','condition':'Poor', 'order': 5}).run(connection)
-        r.db('rcrdkeeprapp').table('record_condition').insert({'abbr':'VP','condition':'Very Poor', 'order': 6}).run(connection)
-        r.db('rcrdkeeprapp').table('record_size').insert({'size':'12 inch', 'order': 1}).run(connection)
-        r.db('rcrdkeeprapp').table('record_size').insert({'size':'10 inch', 'order': 2}).run(connection)
-        r.db('rcrdkeeprapp').table('record_size').insert({'size':'7 inch', 'order': 3}).run(connection)
+        r.db_create(RCRDKEEPER).run(connection)
+        r.db(RCRDKEEPER_DB).table_create('records').run(connection)
+        r.db(RCRDKEEPER_DB).table_create('users').run(connection)
+        r.db(RCRDKEEPER_DB).table_create('record_condition').run(connection)
+        r.db(RCRDKEEPER_DB).table_create('record_size').run(connection)
+        r.db(RCRDKEEPER_DB).table_create('contact').run(connection)
+        r.db('rcrdkeeper').table('record_condition').insert({'abbr':'M','condition':'Mint', 'order': 1}).run(connection)
+        r.db('rcrdkeeper').table('record_condition').insert({'abbr':'VG','condition':'Very Good', 'order': 2}).run(connection)
+        r.db('rcrdkeeper').table('record_condition').insert({'abbr':'G','condition':'Good', 'order': 3}).run(connection)
+        r.db('rcrdkeeper').table('record_condition').insert({'abbr':'AV','condition':'Average', 'order': 4}).run(connection)
+        r.db('rcrdkeeper').table('record_condition').insert({'abbr':'P','condition':'Poor', 'order': 5}).run(connection)
+        r.db('rcrdkeeper').table('record_condition').insert({'abbr':'VP','condition':'Very Poor', 'order': 6}).run(connection)
+        r.db('rcrdkeeper').table('record_size').insert({'size':'12 inch', 'order': 1}).run(connection)
+        r.db('rcrdkeeper').table('record_size').insert({'size':'10 inch', 'order': 2}).run(connection)
+        r.db('rcrdkeeper').table('record_size').insert({'size':'7 inch', 'order': 3}).run(connection)
         print 'Database setup completed. Now run the app without --setup.'
     except RqlRuntimeError:
         print 'App database already exists. Run the app without --setup.'
